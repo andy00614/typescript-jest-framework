563ff150e732aabe5efa90707258d91cdf141a6b
andy00614 <33732843@qq.com>
2020-03-01 15:47:25 +0800

    recover original scaffold

--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -1,4 +1,4 @@
 function greet(name: string) {
-  return name
+  return 'Hello' + ' ' + name
 }
 export { greet };
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
 import { greet } from './demo/demo'
-import { arrOnly } from './arrOnly'
-
-
-const oriArr = [1,2,2,4]
 
 const div = document.createElement('div')
-div.innerHTML = `起始数组是${oriArr}`
-const strong = document.createElement('strong')
-strong.innerHTML = `去重后的数组是${arrOnly(oriArr)}`
-
+div.innerHTML = greet('typescript-jest-framework!')
 
 document.body.appendChild(div)
-document.body.appendChild(strong)
