1d4c0d055d63a919cd34cd38d886accba74096e7
andy00614 <33732843@qq.com>
2020-03-02 10:13:25 +0800

    合并数组

--- src/数组/__tests__/array.test.ts
+++ src/数组/__tests__/array.test.ts
@@ -1,17 +1,26 @@
-import { searchValueIntwoDimensionsArr, find } from "../search";
+import { searchValueIntwoDimensionsArr } from "../search";
+import { mergeArray } from "../merge";
+import { renderNewTwoDimensionArr } from '../../utils/array/array'
 
-// beforeEach(() => {
-//   targetArr = renderNewTwoDimensionArr(unitLength, length);
-// });
+let targetArr:any[] = []
+const unitLength = 5;
+const length = 40
+beforeEach(() => {
+  targetArr = renderNewTwoDimensionArr(unitLength, length);
+});
 
-// test("生成二维数组的方法", () => {
-//   expect(targetArr.length).toBe(length / unitLength);
-// });
+test("生成二维数组的方法", () => {
+  expect(targetArr.length).toBe(length / unitLength);
+});
 
-// test("二维数组中的查找:小肚", () => {
-//   expect(find(targetArr, 60)).toBe(true);
-// });
+test("二维数组中的查找", () => {
+  expect(searchValueIntwoDimensionsArr(targetArr, 30)).toBe(true);
+});
 
-// test("二维数组中的查找:andy", () => {
-//   expect(searchValueIntwoDimensionsArr(targetArr, 60)).toBe(true);
-// });
+test('test marge function',() => {
+  expect(mergeArray([1,8,9],[2,4,12])).toStrictEqual([1,2,4,8,9,12])
+})
+
+test('test marge function2',() => {
+  expect(mergeArray([1,8,9,12,12,12],[2,4,4,12])).toStrictEqual([1,2,4,4,8,9,12,12,12,12])
+})
